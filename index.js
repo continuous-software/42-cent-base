86ce930603d372c940909263e59463bd06f64d00
lorenzofox3 <laurent34azerty@gmail.com>
2014-11-13 11:43:59 +0700

    add builder for complex entities

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ module.exports = {
     BaseGateway: require('./lib/BaseGateway.js'),
     GatewayError: require('./lib/errors.js').GatewayError,
     flat: require('./lib/flat.js'),
-    mapKeys: require('./lib/mapKeys.js')
+    mapKeys: require('./lib/mapKeys.js'),
+    Prospect: require('./lib/prospect.js'),
+    CreditCard: require('./lib/creditCard.js')
 };
