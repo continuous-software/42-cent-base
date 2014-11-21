e2e5d2b229a9893762c12d2c7f2ba64995833110
lorenzofox3 <laurent34azerty@gmail.com>
2014-11-21 18:08:24 +0700

    remove models and utilities from this repository

--- index.js
+++ index.js
@@ -2,10 +2,5 @@
 //todo move Prospect, CreditCard, Subscription plan to a 42-cent-model module
 module.exports = {
     BaseGateway: require('./lib/BaseGateway.js'),
-    GatewayError: require('./lib/errors.js').GatewayError,
-    flat: require('./lib/flat.js'),
-    mapKeys: require('./lib/mapKeys.js'),
-    Prospect: require('./lib/prospect.js'),
-    CreditCard: require('./lib/creditCard.js'),
-    SubscriptionPlan: require('./lib/subscriptionPlan.js')
+    GatewayError: require('./lib/errors.js').GatewayError
 };
